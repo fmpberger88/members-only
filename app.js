791a1c03cb791654eb9d1feb4eb86b7df2acb955
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-14 16:13:11 +0200

    initialize project

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+require('dotenv').config();
+
+// ________________ Environments Variables ________________
+const PORT = process.env.PORT || 5000;
+
+// ________________ Express App ________________
+const app = express();
+
+// // ________________ View Engine ________________
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// ________________ Middlewares ________________
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ________________ Routes ________________
+app.get('/', (req, res, next) => {
+    res.send("Hello World!");
+})
+
+// // ________________ Server ________________
+app.listen(PORT, ()=> {
+    console.log(`Listin on port ${PORT}`)
+})
