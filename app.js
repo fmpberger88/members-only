2f9b9cb68274a7201d9048d3f0dddb1c52fcedbf
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-17 23:38:21 +0200

    push for production

--- app.js
+++ app.js
@@ -78,9 +78,9 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: false,
         maxAge: 30 * 60 * 60 * 1000,
-        sameSite: 'Lax'
+        sameSite: 'Strict'
     }
 }));
 
